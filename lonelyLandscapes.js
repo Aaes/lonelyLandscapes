683ba11379990708cddcb68670efdefdc02b03db
Nikolaj Aaes <nikolajaaes@gmail.com>
2022-03-09 21:42:16 +0100

    Added color swatches

--- lonelyLandscapes.js
+++ lonelyLandscapes.js
@@ -3,8 +3,18 @@ var start = function () {
     var width = canvas.width;
     var height = canvas.height;
     var ctx = canvas.getContext("2d");
-    var skyColorLight = "blue";
-    var skyColorDark = "grey";
+    var skyColor = getRandomColor();
+    var skyColorLight = skyColor;
+    var skyColorDark = shadeColor(skyColor, -50);
+    var hillColor = getRandomColor();
+    var hillColorDark = shadeColor(hillColor, -30);
+    var hillColorDarker = shadeColor(hillColor, -90);
+    var colorSwatches = document.querySelectorAll("#colors div");
+    colorSwatches[0].style.backgroundColor = skyColorLight;
+    colorSwatches[1].style.backgroundColor = skyColorDark;
+    colorSwatches[2].style.backgroundColor = hillColor;
+    colorSwatches[3].style.backgroundColor = hillColorDark;
+    colorSwatches[4].style.backgroundColor = hillColorDarker;
     var region = new Path2D();
     region.arc(width / 2, height / 3, 160, 0, Math.PI * 2);
     ctx.clip(region);
@@ -31,14 +41,20 @@ var start = function () {
         ctx.fillStyle = "white";
         ctx.fillRect(coords.x, coords.y, 1, 1);
     }
-    var hillGradient = ctx.createLinearGradient(width / 2, 0, width / 2, width);
-    hillGradient.addColorStop(0, "red");
-    hillGradient.addColorStop(1, "darkgrey");
+    drawHill(ctx, 200, hillColor, hillColor, width, height);
+    drawHill(ctx, 260, hillColor, hillColorDarker, width, height);
+};
+var drawHill = function (ctx, offset, color, colorLight, width, height) {
+    var hillGradient = ctx.createLinearGradient(width / 2, 200, width / 2, width);
+    hillGradient.addColorStop(0, color);
+    hillGradient.addColorStop(1, colorLight);
     var amplitude = 5;
-    var offsetY = 200;
     var offsetX = 0;
-    var waveLength = 1 / 16;
-    drawFunctionPolygon(0, width, height / 1.5, function (x) { return amplitude * Math.cos(x * waveLength + offsetX) + offsetY; }, ctx, hillGradient);
+    var waveLength = 1 / Math.floor(Math.random() * (24 - 16 + 1) + 16);
+    var drawFunction = function (x) {
+        return amplitude * Math.cos(x * waveLength + offsetX) + offset;
+    };
+    drawFunctionPolygon(0, width, height / 1.6, drawFunction, ctx, hillGradient);
 };
 var drawFunctionPolygon = function (startX, endX, height, lineFunction, ctx, color) {
     ctx.beginPath();
@@ -52,5 +68,28 @@ var drawFunctionPolygon = function (startX, endX, height, lineFunction, ctx, col
     ctx.closePath();
     ctx.fill();
 };
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function shadeColor(color, percent) {
+    var R = parseInt(color.substring(1, 3), 16);
+    var G = parseInt(color.substring(3, 5), 16);
+    var B = parseInt(color.substring(5, 7), 16);
+    R = Math.round((R * (100 + percent)) / 100);
+    G = Math.round((G * (100 + percent)) / 100);
+    B = Math.round((B * (100 + percent)) / 100);
+    R = R < 255 ? R : 255;
+    G = G < 255 ? G : 255;
+    B = B < 255 ? B : 255;
+    var RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+    var GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+    var BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+    return "#" + RR + GG + BB;
+}
 document.addEventListener("DOMContentLoaded", start);
 //# sourceMappingURL=lonelyLandscapes.js.map
--- typescript/lonelyLandscapes.ts
+++ typescript/lonelyLandscapes.ts
@@ -5,8 +5,20 @@ const start = () => {
   const ctx = canvas.getContext("2d");
 
   // choose some colors
-  const skyColorLight = "blue";
-  const skyColorDark = "grey";
+  const skyColor = getRandomColor();
+  const skyColorLight = skyColor;
+  const skyColorDark = shadeColor(skyColor, -50);
+
+  const hillColor = getRandomColor();
+  const hillColorDark = shadeColor(hillColor, -30);
+  const hillColorDarker = shadeColor(hillColor, -90);
+
+  const colorSwatches = document.querySelectorAll("#colors div");
+  (colorSwatches[0] as HTMLDivElement).style.backgroundColor = skyColorLight;
+  (colorSwatches[1] as HTMLDivElement).style.backgroundColor = skyColorDark;
+  (colorSwatches[2] as HTMLDivElement).style.backgroundColor = hillColor;
+  (colorSwatches[3] as HTMLDivElement).style.backgroundColor = hillColorDark;
+  (colorSwatches[4] as HTMLDivElement).style.backgroundColor = hillColorDarker;
 
   // draw the circle cutout
   let region = new Path2D();
@@ -45,25 +57,38 @@ const start = () => {
     ctx.fillStyle = "white";
     ctx.fillRect(coords.x, coords.y, 1, 1);
   }
+
   // optional: draw a moon
 
   // draw the hills
-  const hillGradient = ctx.createLinearGradient(width / 2, 0, width / 2, width);
-  hillGradient.addColorStop(0, "red");
-  hillGradient.addColorStop(1, "darkgrey");
+  drawHill(ctx, 200, hillColor, hillColor, width, height);
+  drawHill(ctx, 260, hillColor, hillColorDarker, width, height);
+};
+
+const drawHill = (
+  ctx: CanvasRenderingContext2D,
+  offset: number,
+  color: string,
+  colorLight: string,
+  width: number,
+  height: number
+) => {
+  const hillGradient = ctx.createLinearGradient(
+    width / 2,
+    200,
+    width / 2,
+    width
+  );
+  hillGradient.addColorStop(0, color);
+  hillGradient.addColorStop(1, colorLight);
 
   const amplitude = 5;
-  const offsetY = 200;
   const offsetX = 0;
-  const waveLength = 1 / 16;
-  drawFunctionPolygon(
-    0,
-    width,
-    height / 1.5,
-    (x) => amplitude * Math.cos(x * waveLength + offsetX) + offsetY,
-    ctx,
-    hillGradient
-  );
+  const waveLength = 1 / Math.floor(Math.random() * (24 - 16 + 1) + 16);
+  const drawFunction = (x: number) =>
+    amplitude * Math.cos(x * waveLength + offsetX) + offset;
+
+  drawFunctionPolygon(0, width, height / 1.6, drawFunction, ctx, hillGradient);
 };
 
 const drawFunctionPolygon = (
@@ -88,5 +113,35 @@ const drawFunctionPolygon = (
   ctx.closePath();
   ctx.fill();
 };
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// from https://stackoverflow.com/a/13532993
+function shadeColor(color: string, percent: number) {
+  let R = parseInt(color.substring(1, 3), 16);
+  let G = parseInt(color.substring(3, 5), 16);
+  let B = parseInt(color.substring(5, 7), 16);
+
+  R = Math.round((R * (100 + percent)) / 100);
+  G = Math.round((G * (100 + percent)) / 100);
+  B = Math.round((B * (100 + percent)) / 100);
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  let RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+  let GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+  let BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+  return "#" + RR + GG + BB;
+}
 
 document.addEventListener("DOMContentLoaded", start);
